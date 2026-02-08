0c5ef632131e32d1e8acaa8c594a067beebbd640
Juha Schacht <juha.schacht@gmail.com>
2026-02-08 07:44:30 -0500

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from constants import *
+from logger import log_state
+
+def main():
+    pygame.init()
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            pass
+        screen.fill("black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
